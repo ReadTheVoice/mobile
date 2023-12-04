94ac42efaab8335fabb362a7b73cbbc00f203ebe
malafrus <malafrus1@gmail.com>
2023-12-04 20:18:46 +0100

    add localization of variables

--- readthevoice/lib/main.dart
+++ readthevoice/lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('fr'),
+          Locale('it')
+        ],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'), //default
+        child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,8 +24,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "ReadTheVoice",
+    return MaterialApp(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Text('Language').tr(),
     );
   }
 }
