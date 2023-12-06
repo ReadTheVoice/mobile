734dc032da3035c1f75dbedbe3a2542b26a42ae2
malafrus <malafrus1@gmail.com>
2023-12-06 22:30:36 +0100

    add QR Code Scanner

--- readthevoice/lib/page/QrCodePage.dart
+++ readthevoice/lib/page/QrCodePage.dart
@@ -1,13 +1,59 @@
-import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
+/*help : 
+    - https://www.youtube.com/watch?v=yj4aNEOun6I*
+    - https://www.youtube.com/watch?v=pKGTKdS8lAs
+    - https://pub.dev/packages/qr_code_scanner
+*/
 
-class QrCodePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QrCodePage extends StatefulWidget {
   const QrCodePage({super.key});
 
   @override
+  State<QrCodePage> createState() => _QrCodePageState();
+}
+
+class _QrCodePageState extends State<QrCodePage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  QRViewController? controller;
+  String result = "";
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData.code!;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Text("QrCodeScan").tr(),
+    return Column(
+      children: [
+        Center(
+          child: SizedBox(
+            width: 300,
+            height: 300,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+        ),
+        Text(
+          result,
+          style:
+              const TextStyle(color: Colors.white, backgroundColor: Colors.red),
+        ),
+      ],
     );
   }
 }
