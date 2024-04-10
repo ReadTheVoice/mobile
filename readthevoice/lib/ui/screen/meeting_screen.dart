ed594ec08cbb3cffc050b5f670378576492f0581
Sharonn <123483236+SharonnElfride@users.noreply.github.com>
2024-04-10 11:24:21 +0200

    scroll auto done

--- readthevoice/lib/ui/screen/meeting_screen.dart
+++ readthevoice/lib/ui/screen/meeting_screen.dart
@@ -6,12 +6,15 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:readthevoice/data/constants.dart';
 import 'package:readthevoice/data/model/meeting.dart';
 import 'package:readthevoice/data/service/firebase_database_service.dart';
 import 'package:readthevoice/data/service/meeting_service.dart';
 import 'package:readthevoice/data/service/transcription_service.dart';
+import 'package:readthevoice/ui/component/basic_components.dart';
 import 'package:readthevoice/ui/component/meeting_basic_components.dart';
 import 'package:readthevoice/utils/utils.dart';
+import 'package:scrollable_text_indicator/scrollable_text_indicator.dart';
 import 'package:toastification/toastification.dart';
 
 class MeetingScreen extends StatefulWidget {
@@ -26,24 +29,17 @@ class MeetingScreen extends StatefulWidget {
 class _MeetingScreenState extends State<MeetingScreen> {
   MeetingService meetingService = const MeetingService();
   FirebaseDatabaseService firebaseService = FirebaseDatabaseService();
-  Stream<DatabaseEvent>? stream;
-  OverlayEntry? detailsOverlayEntry; // Variable to hold the OverlayEntry
   final ScrollController _scrollController = ScrollController();
-  final double _scrollThreshold = 200; // Threshold to show/hide FAB
+  final DatabaseReference transcriptDatabaseReference =
+      FirebaseDatabase.instance.ref(TRANSCRIPT_COLLECTION);
 
   bool hasScroll = false;
-
-  String trying =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut fringilla dolor. Integer dignissim id ipsum id rutrum. Fusce facilisis arcu aliquam gravida pretium. In vulputate, mauris non rhoncus laoreet, orci dolor venenatis nulla, vitae dignissim metus diam ac turpis. Vivamus ut odio vitae arcu lacinia sagittis eget in sapien. Fusce quis accumsan magna. Proin consectetur gravida sapien vel malesuada. Phasellus vel luctus arcu. Quisque consequat placerat nisl non tincidunt. Integer dui massa, venenatis et molestie eget, porttitor eget ligula. Sed ac pulvinar elit. \nCurabitur dictum tortor non neque sagittis placerat. Integer dolor augue, faucibus vel mi ac, ultricies malesuada risus. Donec ut vulputate nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis dui nec porta euismod. Sed consectetur ac enim in imperdiet. Mauris leo tellus, dictum sit amet turpis ut, ultrices consectetur sapien. Fusce rhoncus, ex a vehicula elementum, lorem turpis sollicitudin dui, ut suscipit sem nisi in mi. Vivamus finibus ornare lorem a tristique. Mauris a euismod nisl. Maecenas sodales consectetur sapien, ac malesuada dolor cursus et. In lobortis nisl eu consequat porttitor. Aenean id nibh ornare, mollis erat eget, cursus quam. In hac habitasse platea dictumst. Phasellus consectetur orci at aliquet consequat. \nNam turpis tortor, finibus et interdum sed, semper a nulla. Nulla faucibus, turpis a consectetur ultrices, arcu ante dignissim ante, nec ultrices massa lacus et justo. Nulla rhoncus arcu vel tellus tristique, in placerat est lobortis. Donec quam velit, finibus ac faucibus eu, facilisis quis purus. Suspendisse laoreet aliquam risus, sed viverra orci. Vestibulum eget velit in tortor semper pellentesque et non est. Nam in mollis sem, iaculis scelerisque ipsum. Nunc dictum nulla ut felis gravida, non dictum elit aliquet. Phasellus sodales lacus nunc, vel tincidunt dui commodo vel. Morbi nec quam faucibus, pulvinar turpis nec, maximus metus. Nunc pulvinar nisi non nunc pulvinar elementum.";
 
   Future<void> _getStream() async {
     var transcript = await firebaseService
         .updateLocalMeetingTranscription(widget.meeting.id);
     await meetingService.updateMeetingTranscription(
         widget.meeting.id, transcript);
-
-    stream =
-        await firebaseService.streamMeetingTranscription(widget.meeting.id);
   }
 
   Future<void> _updateTranscription(
@@ -53,7 +49,6 @@ class _MeetingScreenState extends State<MeetingScreen> {
 
   @override
   void initState() {
-    addStreamData2();
     _getStream();
 
     super.initState();
@@ -64,9 +59,6 @@ class _MeetingScreenState extends State<MeetingScreen> {
 
   @override
   void dispose() {
-    detailsOverlayEntry?.dispose();
-    Overlay.of(context).dispose();
-    removeDetailsOverlay(context);
     _scrollController.dispose();
     super.dispose();
   }
@@ -80,29 +72,8 @@ class _MeetingScreenState extends State<MeetingScreen> {
   }
 
   void _scrollToBottom(_) {
-    // print("IN _scrollToBottom 2 + i == ${_scrollController.position.maxScrollExtent}");
-    // print("IN _scrollToBottom 2 + i == ${_scrollController.offset}");
-
     if (!hasScroll) {
       _initScrollToBottom(_);
-    }
-  }
-
-  void removeDetailsOverlay(BuildContext context) {
-    detailsOverlayEntry?.remove();
-    detailsOverlayEntry = null;
-  }
-
-  void _showDetailsOverlay(BuildContext context) {
-    detailsOverlayEntry = OverlayEntry(
-      builder: (context) => DetailsOverlay(
-        onClose: () => removeDetailsOverlay(context),
-        meeting: widget.meeting,
-      ),
-    );
-
-    if (detailsOverlayEntry != null) {
-      Overlay.of(context).insert(detailsOverlayEntry!);
     }
   }
 
@@ -121,35 +92,8 @@ class _MeetingScreenState extends State<MeetingScreen> {
     return result.isGranted;
   }
 
-  Stream<String> addStreamData2() async* {
-    for (int i = 0; i <= 15; i++) {
-      print("IN ADD STREAM 2 + i == $i");
-
-      await Future.delayed(const Duration(seconds: 5));
-      yield "In vulputate, mauris non rhoncus laoreet, orci dolor venenatis nulla, vitae dignissim metus diam ac turpis. \nCurabitur dictum tortor non neque sagittis placerat. Integer dolor augue, faucibus vel mi ac, ultricies malesuada risus. Donec ut vulputate nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-
-    _scrollController.addListener(() {
-      if (_scrollController.offset != _scrollController.position.maxScrollExtent) {
-        // User has scrolled down past the threshold, show the FAB
-        if (!hasScroll) {
-          // Only update state if necessary to avoid unnecessary rebuilds
-          hasScroll = true;
-        }
-      } else {
-        // User is at the top or scrolled up, hide the FAB
-        if (hasScroll) {
-          // Only update state if necessary to avoid unnecessary rebuilds
-          hasScroll = false;
-        }
-      }
-    });
-    
-    var tt = "";
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -197,6 +141,8 @@ class _MeetingScreenState extends State<MeetingScreen> {
                     PopupMenuItem(
                       child: TextButton(
                         onPressed: () async {
+                          await _getStream();
+
                           if (widget.meeting.transcription.trim().isNotEmpty) {
                             bool result = await _permissionRequest();
                             if (result) {
@@ -307,9 +253,11 @@ class _MeetingScreenState extends State<MeetingScreen> {
                     size: 15,
                   ),
                   onPressed: () => {
-                    setState(() {
-                      _showDetailsOverlay(context);
-                    })
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => DetailsOverlay(
+                              onClose: () => Navigator.pop(context),
+                              meeting: widget.meeting,
+                            )))
                   },
                 ),
               ],
@@ -319,139 +267,77 @@ class _MeetingScreenState extends State<MeetingScreen> {
               padding: const EdgeInsets.all(10),
               child: ConstrainedBox(
                   constraints: const BoxConstraints(maxHeight: 650.0),
-                  child: Stack(
-                    children: [
-                      // if (hasScroll)
-                      //   Positioned(
-                      //     bottom: 50, // Adjust as needed
-                      //     right: 16, // Adjust as needed
-                      //     child: FloatingActionButton(onPressed: (){ }, backgroundColor: Colors.black.withOpacity(0.6), child: const FaIcon(FontAwesomeIcons.angleDown),),
-                      //   ),
-                      SingleChildScrollView(
-                        controller: _scrollController,
-                        child: StreamBuilder(
-                            stream: addStreamData2(),
-                            builder: (BuildContext context, snapshot) {
-                              if (snapshot.hasError) {
-                                return const Text("Error");
-                              } else if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const CircularProgressIndicator
-                                    .adaptive();
-                              }
+                  child: (widget.meeting.endDateAtMillis != null ||
+                          widget.meeting.status == MeetingStatus.ended)
+                      ? ScrollableTextIndicator(
+                          text: Text(widget.meeting.transcription),
+                          indicatorBarColor:
+                              Theme.of(context).colorScheme.onBackground,
+                          indicatorThumbColor:
+                              Theme.of(context).colorScheme.onBackground,
+                        )
+                      : SingleChildScrollView(
+                          controller: _scrollController,
+                          child: StreamBuilder(
+                              stream: transcriptDatabaseReference
+                                  .child(widget.meeting.id)
+                                  .onValue,
+                              builder: (BuildContext context, snapshot) {
+                                print("Snapshot".toUpperCase());
+                                print(snapshot);
 
-                              tt += "\n\n${snapshot.data}";
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return const CircularProgressIndicator
+                                      .adaptive();
+                                }
 
-                              if (_scrollController.offset !=
-                                  _scrollController.position.maxScrollExtent) {
-                                hasScroll = true;
-                              } else {
-                                hasScroll = false;
-                              }
+                                if (snapshot.hasData) {
+                                  if (snapshot.data != null &&
+                                      snapshot.data!.snapshot.exists &&
+                                      snapshot.data?.snapshot.value != null) {
+                                    // {data: . Bonjour.. . . Un. . deux. . . }
+                                    dynamic data =
+                                        snapshot.data?.snapshot.value;
 
-                              WidgetsBinding.instance
-                                  .addPostFrameCallback(_scrollToBottom);
+                                    // set transcription
+                                    _updateTranscription(
+                                        data["data"], widget.meeting.id);
 
-                              // return Text(trying + tt);
+                                    if (_scrollController.offset !=
+                                        _scrollController
+                                            .position.maxScrollExtent) {
+                                      hasScroll = true;
+                                    } else {
+                                      hasScroll = false;
+                                    }
 
-                              return Text(trying + tt);
-                            }),
-                      ),
-                    ],
-                  )
+                                    WidgetsBinding.instance
+                                        .addPostFrameCallback(_scrollToBottom);
 
-                  /*
-                (widget.meeting.endDateAtMillis != null ||
-                        widget.meeting.status == MeetingStatus.ended)
-                    ? ScrollableTextIndicator(
-                        text: Text(widget.meeting.transcription),
-                        indicatorBarColor:
-                            Theme.of(context).colorScheme.onBackground,
-                        indicatorThumbColor:
-                            Theme.of(context).colorScheme.onBackground,
-                      )
-                    :
+                                    return Text(
+                                        "${data["data"] ?? "No Transcription"}");
+                                  }
 
-SingleChildScrollView(
-                        // controller: _scrollController,
-                        controller: _scrollController,
-                        child: StreamBuilder(
-                            stream: addStreamData2(),
-                            builder: (BuildContext context, snapshot) {
-                              if (snapshot.hasError) {
-                                return const Text("Error");
-                              } else if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const CircularProgressIndicator
-                                    .adaptive();
-                              }
+                                  if (snapshot.hasError) {
+                                    print("SNAPSHOT ERROR");
+                                    print(snapshot.error);
+                                  }
 
-                              tt += "\n\n${snapshot.data}";
-                              WidgetsBinding.instance
-                                  .addPostFrameCallback(_scrollToBottom);
-
-                              return Text(trying + tt);
-                            }),
-                      )
-                     */
-
-                  // SingleChildScrollView(
-                  //         // controller: _scrollController,
-                  //         child: StreamBuilder(
-                  //             stream: stream,
-                  //             builder: (BuildContext context, snapshot) {
-                  //               if (snapshot.connectionState ==
-                  //                   ConnectionState.waiting) {
-                  //                 return const CircularProgressIndicator
-                  //                     .adaptive(); // adaptive make iOS or android circular progress accordingly
-                  //               }
-                  //
-                  //               if (snapshot.hasData) {
-                  //                 if (snapshot.data != null &&
-                  //                     snapshot.data!.snapshot.exists &&
-                  //                     snapshot.data?.snapshot.value != null) {
-                  //                   // {data: . Bonjour.. . . Un. . deux. . . }
-                  //                   dynamic data =
-                  //                       snapshot.data?.snapshot.value;
-                  //                   // set transcription
-                  //                   _updateTranscription(
-                  //                       data["data"], widget.meeting.id);
-                  //                   // snapshot.data?.snapshot.key);
-                  //
-                  //                   /*
-                  //                     TextScroll(
-                  //                       'This is the sample text for Flutter TextScroll widget. ',
-                  //                       mode: TextScrollMode.bouncing,
-                  //                       velocity: Velocity(pixelsPerSecond: Offset(150, 0)),
-                  //                       delayBefore: Duration(milliseconds: 500),
-                  //                       numberOfReps: 5,
-                  //                       pauseBetween: Duration(milliseconds: 50),
-                  //                       style: TextStyle(color: Colors.green),
-                  //                       textAlign: TextAlign.right,
-                  //                       selectable: true,
-                  //                       )
-                  //                      */
-                  //
-                  //                   return Text(
-                  //                       "${data["data"] ?? "No Transcription"}");
-                  //                 }
-                  //
-                  //                 if (snapshot.hasError) {
-                  //                   print("SNAPSHOT ERROR");
-                  //                   print(snapshot.error);
-                  //                 }
-                  //
-                  //                 return const Center(
-                  //                   child: Text("No Transcription"),
-                  //                 );
-                  //               } else if (snapshot.hasError) {
-                  //                 return Text("Error: \n${snapshot.error}");
-                  //               } else {
-                  //                 return const AppPlaceholder();
-                  //               }
-                  //             }),
-                  //       )
-                  ),
+                                  return const Center(
+                                    child: Text("No Transcription"),
+                                  );
+                                } else if (snapshot.hasError) {
+                                  return Text("Error: \n${snapshot.error}");
+                                } else {
+                                  return (widget.meeting.transcription
+                                          .trim()
+                                          .isNotEmpty)
+                                      ? Text(widget.meeting.transcription)
+                                      : const AppPlaceholder();
+                                }
+                              }),
+                        )),
             ),
             const Spacer(),
             Center(
@@ -467,15 +353,6 @@ SingleChildScrollView(
           ],
         ),
       ),
-      floatingActionButton: hasScroll
-          ? FloatingActionButton(
-              onPressed: () {
-                WidgetsBinding.instance.addPostFrameCallback(_scrollToBottom);
-              },
-              backgroundColor: Colors.grey.shade600,
-              child: const FaIcon(FontAwesomeIcons.angleDown),
-            )
-          : null,
     );
   }
 }
@@ -491,7 +368,6 @@ class DetailsOverlay extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Material(
-      color: Colors.black.withOpacity(0.8), // Semi-transparent background
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20),
