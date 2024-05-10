cc25be7610e2e6290f3ca61ebbd543df8ed4556e
malafrus <malafrus1@gmail.com>
2024-05-10 18:33:07 +0200

    add setting file for theme

--- readthevoice/lib/theme/theme_provider.dart
+++ readthevoice/lib/theme/theme_provider.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:readthevoice/theme/theme.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class ThemeProvider with ChangeNotifier {
+  File fileData = File('');
+
   ThemeData _themeData = lightMode;
 
   ThemeData get themeData => _themeData;
@@ -11,11 +16,40 @@ class ThemeProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void toggleTheme(int theme) {
-    if (theme == 0) {
-      themeData = lightMode;
+  Future<File> initFile() async {
+    final Directory tempDir = await getApplicationDocumentsDirectory();
+    final File file = File('${tempDir.path}/settings.json');
+    return file;
+  }
+
+  Future<void> initTheme() async {
+    fileData = await initFile();
+
+    if (await fileData.exists()) {
+      Map<String, dynamic> data = json.decode(await fileData.readAsString());
+      String theme = data['theme'];
+
+      themeData = theme == 'lightMode' ? lightMode : darkMode;
     } else {
-      themeData = darkMode;
+      Map<String, dynamic> donnees = {'theme': 'lightMode'};
+      await fileData.writeAsString(json.encode(donnees));
     }
+  }
+
+  void saveSettings(int index) async {
+    fileData = await initFile();
+
+    if (await fileData.exists()) {
+      Map<String, dynamic> donnees = {
+        'theme': index == 0 ? 'lightMode' : 'darkMode',
+      };
+      await fileData.writeAsString(json.encode(donnees));
+    }
+  }
+
+  void toggleTheme(int index) async {
+    saveSettings(index);
+
+    themeData = index == 0 ? lightMode : darkMode;
   }
 }
