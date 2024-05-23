bac5e381914479584b7a14605355c438f742d897
Sharonn <sharonn.zounon@clarilog.com>
2024-05-23 17:37:11 +0200

    onboarding screen

--- readthevoice/lib/ui/screen/onboard_screen.dart
+++ readthevoice/lib/ui/screen/onboard_screen.dart
@@ -1,34 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:introduction_screen/introduction_screen.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:readthevoice/ui/screen/master_screen.dart';
-
-class OnBoard {
-  final String image, title, description;
-
-  OnBoard(
-      {required this.image, required this.title, required this.description});
-}
-
-final List<OnBoard> demoData = [
-  OnBoard(
-    image: "",
-    title: "Find Best Musicians All Around Your City",
-    description:
-        "Thousands of musicians around you are waiting to rock your event.",
-  ),
-  OnBoard(
-    image: "",
-    title: "Fastest Way To Book Great Musicians",
-    description:
-        "Find the perfect match to perform for your event and make the day remarkable.",
-  ),
-  OnBoard(
-    image: "",
-    title: "Find Top Sessions Pros For Your Event",
-    description:
-        "Find the perfect match to perform for your event and make the day remarkable.",
-  ),
-];
 
 class OnboardScreen extends StatefulWidget {
   final Function onboardingFinished;
@@ -40,8 +14,6 @@ class OnboardScreen extends StatefulWidget {
 }
 
 class _OnboardScreenState extends State<OnboardScreen> {
-  final introKey = GlobalKey<IntroductionScreenState>();
-
   void _onIntroEnd(context) {
     widget.onboardingFinished();
     Navigator.of(context).pushReplacement(
@@ -49,86 +21,161 @@ class _OnboardScreenState extends State<OnboardScreen> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return IntroductionScreen(
-      key: introKey,
-      globalBackgroundColor: Colors.white,
-      skipStyle: ButtonStyle(
-          textStyle: WidgetStateProperty.all(const TextStyle(fontSize: 17)),
-          foregroundColor: WidgetStateProperty.all(Colors.redAccent)),
-      allowImplicitScrolling: true,
-      autoScrollDuration: 300000,
-      infiniteAutoScroll: true,
-      pages: [
-        PageViewModel(
-          title: "",
-          bodyWidget: const Column(
-            children: [
-              Text('Your Personal Habit Manager',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 248, 64, 64))),
-              SizedBox(height: 20),
-              Image(image: AssetImage('assets/images/chicken-3070851_640.png')),
+  Widget screenBody() {
+    final Color titleColor = Theme.of(context).colorScheme.onBackground,
+        textColor = Theme.of(context).colorScheme.onBackground;
+    final Color backgroundColor = Theme.of(context).colorScheme.background;
+    final Color surfaceColor = Theme.of(context).colorScheme.surface;
+
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return OnBoardingSlider(
+      finishButtonText: 'Get started',
+      onFinish: () => _onIntroEnd(context),
+      finishButtonStyle: FinishButtonStyle(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+      ),
+      skipTextButton: Text(
+        'Skip',
+        style: TextStyle(
+          fontSize: 16,
+          color: titleColor,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailing: const Text(""),
+      controllerColor: isDarkMode ? textColor : surfaceColor,
+      totalPage: 3,
+      headerBackgroundColor: backgroundColor,
+      pageBackgroundColor: backgroundColor,
+      centerBackground: true,
+      background: [
+        SvgPicture.asset(
+          "assets/images/onboarding/QR-code-scanning.svg",
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SvgPicture.asset(
+            "assets/images/onboarding/undraw_speech_to_text_re_8mtf.svg",
+          ),
+        ),
+        SvgPicture.asset(
+          "assets/images/onboarding/Conference-amico.svg",
+        ),
+      ],
+      speed: 1.8,
+      pageBodies: [
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'welcome_title',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: titleColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ).tr(),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'welcome_text',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ).tr(),
             ],
           ),
         ),
-        PageViewModel(
-          title: "",
-          bodyWidget: const Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            // mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image(image: AssetImage('assets/images/chicken-3070851_640.png')),
-              SizedBox(height: 20),
-              Text('Play Guitar!',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 119, 56, 199)))
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'real_time_title',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: titleColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ).tr(),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'real_time_text',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ).tr(),
             ],
           ),
         ),
-        PageViewModel(
-          title: "",
-          bodyWidget: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Listen Music!',
-                  style: TextStyle(
-                      fontSize: 33,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 248, 64, 64))),
-              SizedBox(height: 20),
-              Image(image: AssetImage('assets/images/chicken-3070851_640.png')),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 480,
+              ),
+              Text(
+                'attending_meetings_title',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: titleColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ).tr(),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'attending_meetings_text',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ).tr(),
             ],
           ),
         ),
       ],
-      showSkipButton: true,
-      skipOrBackFlex: 0,
-      nextFlex: 0,
-      // onChange: (val) {},
-      skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
-      next: const Icon(
-        Icons.arrow_forward,
-      ),
-      done: const Text('Done',
-          style: TextStyle(
-              fontWeight: FontWeight.w600,
-              color: Color.fromARGB(255, 248, 64, 64))),
-      onDone: () => _onIntroEnd(context),
-      nextStyle: ButtonStyle(
-          foregroundColor:
-              WidgetStateProperty.all(const Color.fromARGB(255, 248, 64, 64))),
-      dotsDecorator: const DotsDecorator(
-        size: Size.square(10),
-        activeColor: Colors.redAccent,
-        activeSize: Size.square(17),
-      ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return screenBody();
   }
 }
